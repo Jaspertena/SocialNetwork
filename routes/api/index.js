162837dbdbd78af713cb92317ac1429125968f31
Maddox Twin1 <jaspertena@aol.com>
2022-05-20 18:04:17 -0400

    create api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,8 @@
 const router = require('express').Router();
+const thoughtsRoutes = require('./thoughtsRoutes')
+const userRoutes = require('./userRoutes')
+
+route.use('/user', userRoutes);
+route.use('/thoughts', thoughtrRoutes);
+
+module.exports = router;
