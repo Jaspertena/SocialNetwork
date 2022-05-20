9841a121a1e1e02194d43d2a2ead93dafc7d2d18
Maddox Twin1 <jaspertena@aol.com>
2022-05-20 00:31:34 -0400

    update thought.js

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,2 +1,40 @@
 const { Schema, model } = require('mongoose');
 const ThoughtsSchema = require('./Thoughts');
+
+const ThoughtsSchema = new Schema(
+    {
+        thougtText: {
+          type: String,
+          required: true,
+          maxlength: 280,
+          minlength: 1,
+        },
+      
+       createdAt: {
+        createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+        {
+          toJSON: {
+            getters: true,
+          },
+          id: false,
+        },
+
+    username: {
+            type: String,
+            required: true,
+    },
+       
+      friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'Users',
+        },
+    ],
+     },
+
+
+    module.exports = ThoughtsSchema;
